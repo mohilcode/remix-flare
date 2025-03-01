d97fbab2f73645782b9b72e12443b11a8d5b27a2
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2025-03-01 17:18:44 +0900

    ref: admin

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,6 +1,8 @@
+import { adminClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 import { API_BASE_URL } from '../constants/env'
 
 export const authClient = createAuthClient({
   baseURL: API_BASE_URL,
+  plugins: [adminClient()],
 })
